e16bae7653a271f0ef02d3eba5b874f5eb35ebb9
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-10-21 01:28:01 -0500

    added seat features

--- static/index.js
+++ static/index.js
@@ -6,23 +6,85 @@ new Vue({
             check: 0,
             offerToggle: true,
             requestsCount: 2,
-            dialog: false,
-            userName: "First Last",
-            seatNumber: "A3",
-            interests: ["Travelling", "Politics"],
-            seats: [{
-                    "id": 0,
-                    "class": "r"
-                },
-                {
-                    "id": 1,
-                    "class": "g"
-                },
-                {
-                    "id": 2,
-                    "class": "y"
-                }
+            requestDialog: false,
+            interestDialog: false,
+            userName: "FirstName LastName",
+            userSeat: "A3",
+            interests: "Travelling, Politics",
+            newInterest: "",
+            seats: [
+                [
+                    [{
+                            "seatNumber": "A1",
+                            "seatStatus": "R",
+                            "interests": "Cycling"
+                        },
+                        {
+                            "seatNumber": "A2",
+                            "seatStatus": "G",
+                            "interests": "Travelling"
+                        },
+                        {
+                            "seatNumber": "A3",
+                            "seatStatus": "B",
+                            "interests": "Sports, Movies"
+                        }
+                    ],
+                    [{
+                            "seatNumber": "B1",
+                            "seatStatus": "Y",
+                            "interests": "Politics, Technology"
+                        },
+                        {
+                            "seatNumber": "B2",
+                            "seatStatus": "P",
+                            "interests": "Sports, Music"
+                        },
+                        {
+                            "seatNumber": "B3",
+                            "seatStatus": "E",
+                            "interests": "Politics, Travelling"
+                        }
+                    ]
+
+                ],
+                [
+                    [{
+                            "seatNumber": "A1",
+                            "seatStatus": "R",
+                            "interests": "Cycling"
+                        },
+                        {
+                            "seatNumber": "A2",
+                            "seatStatus": "G",
+                            "interests": "Travelling"
+                        },
+                        {
+                            "seatNumber": "A3",
+                            "seatStatus": "B",
+                            "interests": "Sports, Movies"
+                        }
+                    ],
+                    [{
+                            "seatNumber": "B1",
+                            "seatStatus": "Y",
+                            "interests": "Politics, Technology"
+                        },
+                        {
+                            "seatNumber": "B2",
+                            "seatStatus": "P",
+                            "interests": "Sports, Music"
+                        },
+                        {
+                            "seatNumber": "B3",
+                            "seatStatus": "E",
+                            "interests": "Politics, Travelling"
+                        }
+                    ]
+
+                ]
             ]
+
         }
     },
     computed: {
@@ -32,8 +94,15 @@ new Vue({
         }
     },
     methods: {
-        seatClicked() {
-            this.check = 1;
+        seatClicked(seat) {
+            if ((seat.class) == "g")
+                this.check = 1;
+        },
+
+        // this function is called is interest is updated
+        interestAdd() {
+            this.interests += this.newInterest;
+            this.check = this.interests;
         }
     }
 });
